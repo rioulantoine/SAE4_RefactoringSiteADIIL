e861780ccba5abee2400bbc2de7d818ff887ae52
rioulantoine <rioul.antoine@gmail.com>
2026-04-09 15:56:43 +0200

    Changement chronologique actualités

--- src/Model/ModelNews.php
+++ src/Model/ModelNews.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/database.php';
 function getNews(DB $db, int $show): array
 {
     return $db->select(
-        'SELECT id_actualite, titre_actualite, date_actualite FROM ACTUALITE WHERE date_actualite <= NOW() ORDER BY date_actualite ASC LIMIT ?;',
+        'SELECT id_actualite, titre_actualite, date_actualite FROM ACTUALITE WHERE date_actualite <= NOW() ORDER BY date_actualite DESC LIMIT ?;',
         'i',
         [$show]
     );
